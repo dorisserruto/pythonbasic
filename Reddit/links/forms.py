e3be6342c8dff91055936feeac45bd233fe5666d
dorisserruto <doris.serruto.r@gmail.com>
2016-06-12 19:42:00 -0500

    Delete forms.py

--- Reddit/links/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from links.models import Link 
-
-# Create the form class.
-class LinkForm(ModelForm):
-    class Meta:
-        model = Link
-        exclude = ('positive_votes','negative_votes')

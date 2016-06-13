fe454bea394e4ed79ebdc1526a0f3d737c626bfa
dorisserruto <doris.serruto.r@gmail.com>
2016-06-12 19:42:35 -0500

    Delete models.py

--- Reddit/links/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Link(models.Model):
-    title = models.CharField(max_length=200)
-    url = models.URLField(verify_exists=False,max_length=200)
-    positive_votes = models.PositiveIntegerField(default=0)
-    negative_votes = models.PositiveIntegerField(default=0)
